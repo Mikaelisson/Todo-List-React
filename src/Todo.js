3f8a3f8e3605175a99cf88a206e874714cbfe63f
Mikaelisson <mikaelissongesuino@gmail.com>
2022-07-04 22:26:51 -0300

    commit initial

---
+++ src/Todo.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./Todo.css";
+import List from "./components/List";
+import Item from "./components/Item";
+import TodoForm from "./components/TodoForm";
+
+function Todo() {
+  const [items, setItems] = useState([]);
+
+  function onAddItem(text) {
+    let item = new Item(text);
+
+    setItems([...items, item]);
+  }
+
+  function onItemDeleted(item) {
+    let filteredItems = items.filter((it) => it.id !== item.id);
+
+    setItems(filteredItems);
+  }
+
+  function onDone(item) {
+    let updatedItems = items.map((it) => {
+      if (it.id === item.id) {
+        it.done = !it.done;
+      }
+
+      return it;
+    });
+
+    setItems(updatedItems);
+  }
+
+  return (
+    <div className="container">
+      <h1>Todo</h1>
+      <TodoForm onAddItem={onAddItem} />
+      <List onDone={onDone} onItemDeleted={onItemDeleted} items={items} />
+    </div>
+  );
+}
+
+export default Todo;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function TodoForm(props) {
+  const [text, setText] = useState("");
+
+  function hadleChange(event) {
+    let text = event.target.value;
+    setText(text);
+  }
+
+  function addItem(event) {
+    event.preventDefault();
+    if (text) {
+      props.onAddItem(text);
+      setText("");
+    }
+  }
+
+  return (
+    <form>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          onChange={hadleChange}
+          value={text}
+        ></input>
+        <button
+          className="btn btn-light"
+          type="button"
+          onClick={addItem}
+        >
+          Add
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default TodoForm;
